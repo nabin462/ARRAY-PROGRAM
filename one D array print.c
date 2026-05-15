efb32c892b4578b836ed0f31b5a78c4708dfd8e5
nabin462 <budhathokinabin700@gmail.com>
2026-05-15 14:18:14 +0545

    Add files via upload

---
+++ one D array print.c
@@ -0,0 +1,12 @@
+//WAP to print one dimensional array
+#include<stdio.h>
+int main()
+{
+	int a[5]={4,5,6,7,1},i;
+	printf("the array element\n");
+	for(i=0;i<5;i++)
+	{
+		printf("%d\t",a[i]);
+	}
+	return 0;
+}
